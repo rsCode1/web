fcda5aa022f8a9998609d353ee9a3eb0e29d09a3
Roi Shamayev <roi222112@gmail.com>
2024-01-17 17:01:40 +0200

    first commit

---
+++ set_git.bat
@@ -0,0 +1,11 @@
+:: this bat file is used to set git repository
+:: to run use .\set_git.bat
+@echo off
+:: add :: before the next row to initialize the repository
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rsCode1/web.git
+git push -u origin main
